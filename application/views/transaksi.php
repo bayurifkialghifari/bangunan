134a134ba8bfd0c581ce8e9f07edb81d86a8797e
Bayu <bayurifkialgh@gmail.com>
2020-06-26 17:22:52 +0700

    OK

--- application/views/transaksi.php
+++ application/views/transaksi.php
@@ -13,7 +13,7 @@
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <!-- CSS Files -->
+    <!-- CSS Files ASD-->
     <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="<?= base_url(); ?>assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
     <!-- CSS Just for demo purpose, don't include it in your project -->
